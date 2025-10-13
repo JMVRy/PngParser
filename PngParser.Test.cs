0dfd29ec3f572c5e846a2c871bab1f98df9e9e8b
JMVRy <77213524+JMVRy@users.noreply.github.com>
2025-10-13 06:44:17 -0500

    Changing test to use `Assert.DoesNotThrow` for self-explanatory expectation of result

--- PngParser.Test.cs
+++ PngParser.Test.cs
@@ -8,8 +8,11 @@ public class PngParserTest
     [Test]
     public void TestValidPng()
     {
-        byte[] validPng = [ 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a ];
-        PngParser.PngParser.Parse( validPng );
+        Assert.DoesNotThrow( () =>
+        {
+            byte[] validPng = [ 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a ];
+            PngParser.PngParser.Parse( validPng );
+        } );
     }
 
     [Test]
